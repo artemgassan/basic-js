4b6e5054802630d1f8382c859e2e2aa523b09490
artemgassan <artemgassan@ya.ru>
2025-01-04 18:36:11 +0300

    feat: finish minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const resArr = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    resArr[i] = [];
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      const left = j - 1 >= 0 ? matrix[i][j - 1] : null;
+      const right = j + 1 < matrix[i].length ? matrix[i][j + 1] : null;
+      const up = i - 1 >= 0 ? matrix[i - 1][j] : null;
+      const down = i + 1 < matrix.length ? matrix[i + 1][j] : null;
+      const upLeft = j - 1 >= 0 && i - 1 >= 0 ? matrix[i - 1][j - 1] : null;
+      const upRight =
+          j + 1 < matrix[i].length && i - 1 >= 0 ? matrix[i - 1][j + 1] : null;
+      const downLeft =
+          j - 1 >= 0 && i + 1 < matrix.length ? matrix[i + 1][j - 1] : null;
+      const downRight =
+          j + 1 < matrix[i].length && i + 1 < matrix.length
+              ? matrix[i + 1][j + 1]
+              : null;
+
+      resArr[i][j] = [
+        left,
+        right,
+        up,
+        down,
+        upLeft,
+        upRight,
+        downLeft,
+        downRight,
+      ].reduce((acc, b) => acc + Number(b), 0);
+    }
+  }
+
+  return resArr;
 }
 
 module.exports = {
